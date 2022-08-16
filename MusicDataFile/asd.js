0d4de37eff79f634357cbb604d4d2e3c51ec5217
Renish <82503425+renish12@users.noreply.github.com>
2022-08-16 14:56:01 +0530

    Create asd.js

---
+++ MusicDataFile/asd.js
@@ -0,0 +1,11 @@
+{
+    id: '2',
+    url: 'https://www.bensound.com/bensound-music/bensound-anewbeginning.mp3',
+    title: 'Punky',
+    artist: 'Benjamin Tissot',
+    album: "Bensound's rock",
+    genre: 'Rock',
+    date: '2014-05-20T07:00:00+00:00', // RFC 3339
+    artwork: 'https://www.bensound.com/bensound-img/punky.jpg',
+    duration: 126, // Duration in seconds
+  },
